e32530e0d8a65f2da050d06fe681b658f879356e
kaier <kaier@steam001.com>
2020-11-26 17:10:31 +0800

    wanshan

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const program = require('commander')
 
 const helpOptions = require('./lib/core/help')
+const createCommands = require('./lib/core/createCommands')
+
 //查看版本号
 program.version(require('./package.json').version)
 
 //帮助和可选信息
 helpOptions()
+
+//其他指令
+createCommands()
 
 //传参解析
 program.parse(process.argv)
