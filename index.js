e7dc9afba17a8330d4b5ee302029816a69a41183
kaier <kaier@steam001.com>
2020-11-27 17:11:29 +0800

    完善

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const program = require('commander')
 
 const helpOptions = require('./lib/core/help')
-const createCommands = require('./lib/core/createCommands')
+const createCommands = require('./lib/core/create')
 
 //查看版本号
 program.version(require('./package.json').version)
